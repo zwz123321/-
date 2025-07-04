a368f8b438d3522cad82a948f77fdcaf25d9ec42
zwz123321 <148608754+zwz123321@users.noreply.github.com>
2025-07-04 22:27:18 +0800

    Delete data_analysis.py

--- data_analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# 读取数据
-train_data = pd.read_csv('加州房价预测/data/train.csv')
-
-total = train_data.isnull().sum().sort_values(ascending=False)
-percent =(train_data.isnull().sum()/train_data.isnull().count()).sort_values(ascending=False)
-missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-print(missing_data.head(20))
